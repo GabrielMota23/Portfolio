4953551d4961cf53d79911a8675f6e2ea09337a7
Gabriel Mota <gabrielmotagit@gmail.com>
2024-10-17 20:36:51 -0300

    .

---
+++ portfolio/js.js
@@ -0,0 +1,15 @@
+        var a = window.document.getElementById('area')
+        function clicar(){
+            a.innerText = 'clicou!'
+            a.style.background = "red"
+        }
+
+        function entrar(){
+            a.innerText = 'entrou!'
+        }
+
+        function sair(){
+            a.innerText = 'saiu!'
+            a.style.background = 'green'
+        }
+
